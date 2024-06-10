dc3590deea016a79c7e978d5e176e9aac11230f2
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-10 15:37:43 -0300

    alteracao

--- Desenvolvimento Python/nada/nada.py
+++ Desenvolvimento Python/nada/nada.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("qual o maior ?")
+x = int(input("insira um número :"))
+y = int(input("insira outro :"))
+if y>x:
+    print(f"{y} é maior do que {x}")
+elif x==y:
+    print("iguais")
+else:
+    print(f"{x} é maior que {y}")
