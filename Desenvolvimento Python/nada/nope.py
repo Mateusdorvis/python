4c052b105b1380d552036ebac5753358a9305043
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-12 15:50:01 -0300

    nada

--- Desenvolvimento Python/nada/nope.py
+++ Desenvolvimento Python/nada/nope.py
@@ -1,5 +1,24 @@
 #condição exercíos 
+class Tubarao:
+    animal_tipo = "fish"
+    location = "oceano"
+    flo = 0
 
-class Pessoa: 
-    
+    def __init__(self, nome, tipo):
+         self.nome = nome
+         self.tipo = tipo
+
+    def set_fo(self, flowers):
+        self.flo = flowers
+        print("Nada " + str(flowers) + "follwers")
+
+
+sammy = Tubarao("olá", 4)
+print(sammy.nome)
+print( sammy.location)
+
+stvie = Tubarao("nada", 4)
+print(stvie.nome )
+stvie.set_fo(77)
+print(stvie.animal_tipo)
 
