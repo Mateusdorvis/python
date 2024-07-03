cd4ed036b19c10416981a75b0bd3d94e022b438a
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-03 14:41:20 -0300

    ok

--- atividades/atividade6.py
+++ atividades/atividade6.py
@@ -9,27 +9,27 @@ texto.pack()
 
 
 #alterando a cor para vermelho
-btn1 = tkt.Button(window, text="alterar a janela o plano de fundo vermelho", command=lambda : window.config(background="#c21a01")) #lambada susbtiui a função que queria criar (def alterar_cor), na qual renderia linhas de códigos
+btn1 = tkt.Button(window, text="alterar a janela o plano de fundo  paravermelho", command=lambda : window.config(background="#c21a01")) #lambada susbtiui a função que queria criar (def alterar_cor), na qual renderia linhas de códigos
 btn1.pack(side="top")
 
 #alternado a cor de fundo para amarelo
-btn2 = tkt.Button(window, text="alterar a janela o plano de fundo amarelo", command=lambda : window.config(background="#fcaf14"))
+btn2 = tkt.Button(window, text="alterar a janela o plano de fundo para amarelo", command=lambda : window.config(background="#fcaf14"))
 btn2.pack(side="top")
 
 #alternado a cor de fundo para azul
-btn3 = tkt.Button(window, text="alterar a janela o plano de fundo azul", command=lambda : window.config(background="#066699"))
+btn3 = tkt.Button(window, text="alterar a janela o plano de fundo  paraazul", command=lambda : window.config(background="#066699"))
 btn3.pack(side="top")
 
 #alternado a cor de fundo para laranja
-btn4 = tkt.Button(window, text="alterar a janela o plano de fundo laranja", command=lambda : window.config(background="#e4491c"))
+btn4 = tkt.Button(window, text="alterar a janela o plano de fundo  paralaranja", command=lambda : window.config(background="#e4491c"))
 btn4.pack(side="top")
 
 #alternado a cor de fundo para verde
-btn5 = tkt.Button(window, text="alterar a janela o plano de fundo verde", command=lambda : window.config(background="#aedd2b"))
+btn5 = tkt.Button(window, text="alterar a janela o plano de fundo para verde", command=lambda : window.config(background="#aedd2b"))
 btn5.pack(side="top")
 
 #alternado a cor de fundo para marrom
-btn5 = tkt.Button(window, text="alterar a janela o plano de fundo marrom", command=lambda : window.config(background="#a04b26"))
+btn5 = tkt.Button(window, text="alterar a janela o plano de fundo para marrom", command=lambda : window.config(background="#a04b26"))
 btn5.pack(side="top")
 
 #alternado a cor de fundo para padrão
@@ -37,7 +37,7 @@ btn6 = tkt.Button(window, text="alterar a janela o plano de fundo para padrão",
 btn6.pack(side="top")
 
 #alternado a cor de fundo para preto
-btn7 = tkt.Button(window, text="alterar a janela o plano de fundo preto", command=lambda : window.config(background="black"))
+btn7 = tkt.Button(window, text="alterar a janela o plano de fundo para preto", command=lambda : window.config(background="black"))
 btn7.pack(side="top")
 
 window.mainloop()
