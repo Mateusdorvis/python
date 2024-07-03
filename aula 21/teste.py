bc6ef4468faf7fbc1da670c59b3b72537adf5381
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-03 15:04:41 -0300

    ok

--- aula 21/teste.py
+++ aula 21/teste.py
@@ -9,6 +9,6 @@ label1 = tkt.Label(main, text="Sou label 1", font="Arial", background="red")
 label2 = tkt.Label(main, text="Sou label 2", font="Arial", background="orange")
 
 label1.grid(row=1, column=0)
-label2.grid(row=1, column=2)
+label2.grid(row=9, column=0) #neste caso não existe coluna 2,3 e em diante, mesma coisa que a linha (row)
 
 main.mainloop()
