9dc508a7864d068473ee2b104d5af37f9df965f2
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-04 15:36:43 -0300

    ok

--- aula 21/sorriso.py
+++ aula 21/sorriso.py
@@ -4,3 +4,31 @@ from tkinter import ttk
 janela = tk.Tk()
 janela.title("Sorriso")
 
+q1a= tk.Frame(janela, bg="black", width=100, height=100)
+q1a.place(x=550, y=500)
+
+q1 = tk.Frame(janela, bg="black", width=100, height=100)
+q1.place(x=600, y=550)
+
+q2 = tk.Frame(janela, bg="black", width=100, height=100)
+q2.place(x=800, y=550)
+
+q3 = tk.Frame(janela, bg="black", width=100, height=100)
+q3.place(x=700, y=700)
+
+q4 = tk.Frame(janela, bg="black", width=100, height=100)
+q4.place(x=600, y=800)
+
+q5 = tk.Frame(janela, bg="black", width=100, height=100)
+q5.place(x=800, y=800)
+
+q6 = tk.Frame(janela, bg="black", width=100, height=100)
+q6.place(x=850, y=850)
+
+
+
+
+
+
+
+janela.mainloop()
