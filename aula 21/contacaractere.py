28938c4c4b1931383f8a937364a63e915f4d64c0
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-05 16:55:12 -0300

    ok

---
+++ aula 21/contacaractere.py
@@ -0,0 +1,30 @@
+import tkinter as t
+import os
+from tkinter import ttk
+os.system('cls')
+main = t.Tk()
+label = t.Label(main, text="digite algo")
+label.pack()
+#gerar um container (widget)
+texto = t.Text(main, font="Arial")
+texto.pack()
+
+#teste sem def
+def verifica(verifica):
+    msg = t.Label(main)
+    pegue_o_texto = texto.get('1.0','22.0')
+    leitura = len(pegue_o_texto)
+    if leitura>20:
+        texto['state'] = 'disabled'
+        msg.config(text="ultrapassou")
+        msg.pack()
+        
+    else:
+        msg.config(text="continue escrevendo")
+        msg.pack()
+
+# realizar <KeyRealase>
+
+texto.bind("<KeyRelease>", verifica)
+    
+main.mainloop()
