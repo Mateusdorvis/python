48698936df487e326d39ecd3e42655d6b90331f6
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-08 15:56:17 -0300

    ok

--- aula 21/desafio2.py
+++ aula 21/desafio2.py
@@ -9,6 +9,16 @@ main.title("desafio ")
 main.geometry("500x500")
 label = tk.Label(main, text="escreva algo na caixa de texto")
 label.pack()
+
 text = tk.Text(main)
 text.pack()
+
+def verificar(teclado):
+    pega_o_texto = text.get("1.0", "22.0")
+    if len(pega_o_texto)>20:
+        text['state'] = 'disabled'
+    else:
+        mgs = tk.Label(main, text="continue escrevendo")
+        mgs.pack()
+text.bind("<KeyRelease>", verificar)
 main.mainloop()
