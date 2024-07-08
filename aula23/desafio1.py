5450e72154646df5ab514c2b8a113d6faf5e892c
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-08 17:26:56 -0300

    ok

--- aula23/desafio1.py
+++ aula23/desafio1.py
@@ -2,10 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 import os
 
-
+os.system('cls')
 principal = tk.Tk()
 principal.title("Coletando dados do usuário")
-
 #função para realizar perguntas 
 
 def coleta_dados_para_mim():
@@ -18,15 +17,17 @@ def coleta_dados_para_mim():
     nome_do_usuario()
     #pergunta a idade
 
-    def idade_do_usuario(event):
+    def idade_do_usuario():
         idade = tk.Label(principal, text="Digite sua idade :")
         idade.pack()
         idade_entrada = tk.Entry(principal, width=20)
         idade_entrada.pack()
-    idade_do_usuario
-        
-    
+    idade_do_usuario()
 
+crollbar = ttk.Scrollbar(principal, orient='vertical', command=coleta_dados_para_mim)
+crollbar.pack(side="left", expand=True)
+
+    
 coleta_dados_para_mim()
 
 principal.mainloop()
