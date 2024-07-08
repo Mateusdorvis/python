994da8ccf5cb610b9596c814420cd296038c9f21
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-08 15:44:56 -0300

    ok

--- aula 21/desafio2.py
+++ aula 21/desafio2.py
@@ -1,2 +1,14 @@
-print("testando em 1, 2, 3, 4, 5")
-print("olá olá olá")
+#bota~o que reseta o texto
+
+
+import tkinter as tk
+from tkinter import ttk
+
+main = tk.Tk()
+main.title("desafio ")
+main.geometry("500x500")
+label = tk.Label(main, text="escreva algo na caixa de texto")
+label.pack()
+text = tk.Text(main)
+text.pack()
+main.mainloop()
