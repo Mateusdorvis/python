f36a2cca9a81b11aa2fb7564d756c8b610e26fd6
Mateusdorvis <mateusdorvis004@gmail.com>
2024-07-15 14:31:03 -0300

    ok

--- banco de dados/controller/controller.py
+++ banco de dados/controller/controller.py
@@ -1,10 +1,5 @@
-import tkinter as tk
-
-from model.model import UsuarioModel
-from view.user_v import UsuarioView
-from tkinter import messagebox
 class UsuarioController:
-    def __init__(self, view, model: UsuarioModel): #o parametro view é do tipo a classe UsuarioModel 
+    def __init__(self, view, model):
         self.view = view
         self.model = model
         self.view.adicionar_button.config(command=self.adicionar_usuario)
@@ -18,11 +13,6 @@ class UsuarioController:
             self.view.adicionar_usuario_lista((None, nome, idade))
             self.view.nome_entry.delete(0, tk.END)
             self.view.idade_entry.delete(0, tk.END)
-        else:
-            def mostrar_aviso():
-                messagebox.showinfo("Aviso", "Esta é uma mensagem de aviso!")
-            mostrar_aviso()
-
 
     def carregar_usuarios(self):
         usuarios = self.model.selecionar_usuarios()
